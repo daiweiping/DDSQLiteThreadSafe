366eb1484f9ddb6d523f1e795caf603164944a30
daiweiping <daiweiping914@sina.com>
2016-03-31 14:38:14 +0800

    setup

---
+++ DDSQLiteThreadSafe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DDSQLiteThreadSafe
+//
+//  Created by MIMO on 16/3/31.
+//  Copyright © 2016年 MIMO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DDSQLiteThreadSafe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DDSQLiteThreadSafe
+//
+//  Created by MIMO on 16/3/31.
+//  Copyright © 2016年 MIMO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
